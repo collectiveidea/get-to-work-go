ed1f4d7b88a9863a20a1d61e5df2d0a2c536afa0
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2017-11-06 13:22:23 -0500

    Add open command.
    Opens pivotal tracker project.

---
+++ src/get-to-work/commands/open.go
@@ -0,0 +1,28 @@
+package commands
+
+import (
+	"fmt"
+	"get-to-work/config"
+	"github.com/fatih/color"
+	"github.com/urfave/cli"
+	"os/exec"
+)
+
+// Open prepares the project directory for use
+var Open = cli.Command{
+	Name:  "open",
+	Usage: "Open a web browser showing the current story you're working on",
+	Action: func(c *cli.Context) (err error) {
+		cfg, err := config.DefaultConfig()
+		if err != nil {
+			color.Red("Could not open configuration file")
+			return
+		}
+
+		// How do we get a subdomain for Harvest?
+		ptURL := fmt.Sprintf("https://www.pivotaltracker.com/n/projects/%s", cfg.PivotalTracker.ProjectID)
+		cmd := exec.Command("open", ptURL)
+		cmd.Run()
+		return
+	},
+}
