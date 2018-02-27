ef2f6b85592e84a55392bf37a7ec7bde9db45a89
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2018-02-27 14:34:40 -0500

    Change vendor directory w/

--- vendor/src/github.com/pivotal/gumshoe/trackerapi/domain/project.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type Project struct {
-    ID               int
-    Name             string
-    Description      string
-    CurrentIteration int
-}
--- vendor/src/github.com/pivotal/gumshoe/trackerapi/trackerapi.go
+++
@@ -1,7 +0,0 @@
-package trackerapi
-
-func handleError(err error) {
-    if err != nil {
-        panic(err)
-    }
-}
